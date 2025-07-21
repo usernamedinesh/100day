04402f375483076485f38eae75fa373f5744e49d
usernamedinesh <borod9200@gmail.com>
2025-07-21 22:37:03 +0530

    linked list done

--- day2/main.c
+++ day2/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>  // for malloc
 
 struct node {
     void *data;
@@ -6,7 +7,7 @@ struct node {
 };
 
 struct node *linked_list_new();
-int linked_list_add(struct node *list, void *data);
+int linked_list_append(struct node *list, void *data);
 int linked_list_insert(struct node *list, void *data, int index);
 int linked_list_remove(struct node *list, int index);
 
@@ -16,11 +17,50 @@ struct node *linked_list_new() {
         return NULL;
     }
 
-    list ->data = NULL;
-    list ->next = NULL;
+    list->data = NULL;
+    list->next = NULL;
     return list;
 }
 
-int main() {
-    printf("hellow");
+int linked_list_append(struct node *list, void *data) {
+    struct node *new_node = malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        return -1;
+    }
+
+    new_node->data = data;
+    new_node->next = NULL;
+
+    // Traverse to the end
+    struct node *current = list;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = new_node;
+    return 0;
 }
+
+int main() {
+    struct node *list = linked_list_new();
+    if (list == NULL) {
+        printf("Failed to create list\n");
+        return 1;
+    }
+
+    int value1 = 10;
+    int value2 = 20;
+
+    linked_list_append(list, &value1);
+    linked_list_append(list, &value2);
+
+    // Print the list
+    struct node *current = list->next;  // skip dummy head
+    while (current != NULL) {
+        printf("Value: %d\n", *(int *)current->data);
+        current = current->next;
+    }
+
+    return 0;
+}
+
