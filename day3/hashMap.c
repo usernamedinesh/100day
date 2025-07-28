0762884ed9a8ef4ee7a4f1f4bff6944f78d4e7fe
usernamedinesh <borod9200@gmail.com>
2025-07-28 17:56:10 +0530

    another hash map implementation

---
+++ day3/hashMap.c
@@ -0,0 +1,8 @@
+// another hash map implementation
+#include <stdio.h>
+
+
+int main() {
+    printf("hellow \n");
+    return 0;
+}
--- day3/main.c
+++ day3/main.c
@@ -1,6 +1,54 @@
+// old one 
 #include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char* key;
+    char* value;
+} ht_item;
+
+typedef struct {
+    int size;
+    int cout;
+    ht_item** items;
+} ht_hash_table;
+
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->value = strdup(v);
+    return i;
+}
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size,  sizeof(ht_items*));
+    return ht;
+}
+
+static void ht_del_item(ht_item* i) {
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+void ht_del_hash_table(ht_hash_table* ht) {
+    for (int i = 0; i < ht->size; i++){
+        ht_item* item = ht->items[i];
+            if (item != NULL) {
+            ht_del_item(item);
+        }
+        free(ht->items);
+        free(ht);
+    }
+}
+
+
 int main() {
     printf("hash tables\n");
     return 0;
 }
-
