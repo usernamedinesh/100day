33d08f59d476c72b68dea1ddc026fc62597df415
usernamedinesh <borod9200@gmail.com>
2025-07-28 18:15:32 +0530

    just implementin lieanr search for hashMap

--- day3/hashMap.c
+++ day3/hashMap.c
@@ -1,8 +1,35 @@
 // another hash map implementation
 #include <stdio.h>
+#include <string.h>
 
 
+typedef struct {
+    char* key;
+    int value;
+} item;
+
+item* linear_search(item* items, size_t size, const char* key) {
+    for (int i=0; i<size; ++i){
+        //comparing strings
+        if (strcmp(items[i].key, key) == 0) {
+            return &items[i];
+        }
+    }
+    return NULL;
+}
+
+// lets first imlement linear seaach 
 int main() {
-    printf("hellow \n");
+    item items[] = {
+        { "foo", 10}, {"bar", 42}, {"bazz", 34}, {"bob", 33}, {"x", 35}
+    };
+    size_t s = sizeof(items)/sizeof(item); 
+    item* found = linear_search(items, s, "foo");
+    if(!found) {
+        printf("not found");
+    } else {
+        printf("found");
+    }
+    printf(" \n");
     return 0;
 }
